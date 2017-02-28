04740d16b78dfcde3a7d3d36062786bf56f025a8
lost <lostname12346@gmail.com>
2017-02-28 16:05:32 +0500

    change led priority

--- services/core/java/com/android/server/lights/LightsManager.java
+++ services/core/java/com/android/server/lights/LightsManager.java
@@ -20,8 +20,8 @@ public abstract class LightsManager {
     public static final int LIGHT_ID_BACKLIGHT = 0;
     public static final int LIGHT_ID_KEYBOARD = 1;
     public static final int LIGHT_ID_BUTTONS = 2;
-    public static final int LIGHT_ID_BATTERY = 3;
-    public static final int LIGHT_ID_NOTIFICATIONS = 4;
+    public static final int LIGHT_ID_BATTERY = 4;
+    public static final int LIGHT_ID_NOTIFICATIONS = 3;
     public static final int LIGHT_ID_ATTENTION = 5;
     public static final int LIGHT_ID_BLUETOOTH = 6;
     public static final int LIGHT_ID_WIFI = 7;
